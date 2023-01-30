fd0e383966288f76069449a4f532a640071dd4a4
csd4417 <csd4417@csd.uoc.gr>
2023-01-30 18:02:35 +0200

    Changes to the class

--- Megaventory_Project/Product.hpp
+++ Megaventory_Project/Product.hpp
@@ -1,34 +1,24 @@
 #include <string>
 
-class ProductUnitCostObj {
-    std::string CompanyName;
-    std::string CompanyCurrencyCode;
-    unsigned ProductUnitCost;
-public:
-    ProductUnitCostObj(std::string cname, std::string ccode, unsigned pcost) :
-        CompanyName(cname), CompanyCurrencyCode(ccode), ProductUnitCost(pcost) {}
-    ~ProductUnitCostObj() {}
-};
-
 class Product {
     unsigned ProductID;
     std::string ProductType;
     std::string ProductSKU;
-    std::string ProductEAN;
-    std::string ProductDescription;
-    std::string ProductVersion;
-    std::string ProductLongDescription;
-    unsigned ProductCategoryID;
-    std::string ProductUnitOfMeasurement;
-    unsigned ProductSellingPrice;
-    unsigned ProductPurchasePrice;
-    ProductUnitCostObj ProductUnitCost;
-    unsigned ProductWeight;
-    unsigned ProductWeightUnitID;
-    unsigned ProductLength;
-    unsigned ProductBreadth;
-    unsigned ProductHeight;
-    std::string ProductImageURL;
+    std::string ProductEAN="";
+    std::string ProductDescription="newproductdescription";
+    std::string ProductVersion="";
+    std::string ProductLongDescription="";
+    unsigned ProductCategoryID=0;
+    std::string ProductUnitOfMeasurement="" ;
+    double ProductSellingPrice;
+    double ProductPurchasePrice;
+    double ProductUnitCost=0;
+    unsigned ProductWeight=0;
+    unsigned ProductWeightUnitID=0;
+    unsigned ProductLength=0;
+    unsigned ProductBreadth=0;
+    unsigned ProductHeight=0;
+    std::string ProductImageURL="";
     std::string ProductComments="";
     std::string ProductCustomField1 = "";
     std::string ProductCustomField2 = "";
@@ -40,11 +30,11 @@ class Product {
     std::string ProductCustomField8 = "";
     std::string ProductCustomField9 = "";
     std::string ProductCustomField10 = "";
-    unsigned ProductMainSupplierID;
-    unsigned ProductMainSupplierPrice;
+    unsigned ProductMainSupplierID=0;
+    double ProductMainSupplierPrice=0;
     std::string ProductMainSupplierSKU = "";
     std::string ProductMainSupplierDescription = "";
-    std::string ProductCreationDate;
+    std::string ProductCreationDate="/Date(1507650714280 - 0000)/";
     bool ProductOption1=false;
     bool ProductOption2=false;
     bool ProductOption3=false;
@@ -55,19 +45,19 @@ class Product {
     bool IsPurchasable=true;
     bool IsSellable=true;
 public:
-    Product(unsigned pid, std::string ptype, std::string psku, std::string pean, std::string pdesc, std::string pver,
-        std::string plongdesc, unsigned pcatid, std::string punmes, unsigned pselpr, unsigned ppurpr, std::string cname,
-        std::string ccode, unsigned puncost, unsigned pweight, unsigned pweightid, unsigned plength, unsigned pbr,
-        unsigned pheight, std::string pURL, std::string pcomments, unsigned pmainsupid, unsigned pmainsuppr, std::string pmainsupsku,
-        std::string pmainsupdesc, std::string pcreatdate)
-        :
-        ProductID(pid), ProductType(ptype), ProductSKU(psku), ProductEAN(pean), ProductDescription(pdesc),
-        ProductVersion(pver), ProductLongDescription(plongdesc), ProductCategoryID(pcatid),
-        ProductUnitOfMeasurement(punmes), ProductSellingPrice(pselpr), ProductPurchasePrice(ppurpr),
-        ProductUnitCost(ProductUnitCostObj(cname, ccode, puncost)), ProductWeight(pweight),
-        ProductWeightUnitID(pweightid), ProductLength(plength), ProductBreadth(pbr),
-        ProductHeight(pheight), ProductImageURL(pURL), ProductComments(pcomments),
-        ProductMainSupplierID(pmainsupid), ProductMainSupplierPrice(pmainsuppr),
-        ProductMainSupplierSKU(pmainsupsku), ProductMainSupplierDescription(pmainsupdesc),
-        ProductCreationDate(pcreatdate) {}
+    Json::Value ProductToJSON(APIKEY key) {
+        Json::Value newProduct;
+        newProduct["APIKEY"] = key;
+        newProduct["mvProduct"]["ProductSKU"] = ProductSKU;
+        newProduct["mvProduct"]["ProductType"] = ProductType;
+        newProduct["mvProduct"]["ProductDescription"] = ProductDescription;
+        newProduct["mvRecordAction"] = "Insert";
+        return newProduct;
+    };
+    Product(unsigned pid,std::string psku,std::string ptype, double pselpr, double ppurpr)
+    :ProductID(pid), ProductSKU(psku), ProductType(ptype), ProductSellingPrice(pselpr), ProductPurchasePrice(ppurpr) {
+    }
+    ~Product() {}
+
 };
+
